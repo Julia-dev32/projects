955a35cbde0f80d9ac9a83000686e768e8291b53
Julia-dev32 <juliheart51@yahoo.com>
2025-01-23 13:45:59 -0600

    Delete while loop.py

--- while loop.py
+++
@@ -1,5 +0,0 @@
-substance = float(input("Enter the degrees the substance is"))
-while substance > 102.5:
-    print("Turn down vat's thermostat and wait 5 minutes")
-    substance = float(input("Enter the degrees the substance is"))
-print("This temp is acceptable")
